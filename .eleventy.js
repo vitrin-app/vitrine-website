ba129c33b986b882ee3dc08b51890cd53b9e0325
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2022-06-05 17:36:03 +0200

    update privacy policy

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+const md = require('markdown-it')
+const anchor = require('markdown-it-anchor')
+const slugify = require('slugify')
+
 module.exports = config => {
   config.addPassthroughCopy('src/CNAME')
   config.setTemplateFormats([
@@ -10,6 +14,18 @@ module.exports = config => {
     'svg',
     'ttf',
   ])
+
+  config.setLibrary('md', md({
+    html: true,
+    linkify: true,
+    breaks: true,
+  }).use(anchor, {
+    slugify: s => slugify(s, {
+      lower: true,
+      remove: /\d/,
+      strict: true,
+    })
+  }))
 
   return {
     dir: {
